cd57c13e8e3abfc215be462ac7b06f32d43b32c8
Barkley Santo <barkley.santo@gmail.com>
2021-05-14 15:39:45 -0500

    debug function names in export statements for producrReducer

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,15 +1,17 @@
 import { ActionTypes } from "../constants/action-types";
 
 const initialState = {
-  products =[{
+  products: [{
     id: 1,
     title: "Barkley",
     category: "coder",
-  }], //not sure if the comma is needed
+  }] //not sure if the comma is needed
 };
+
+
 // reducer always takes the initial state and the action
-export const productReducer = (state, { type, payload }) => {
-  switch (action.type) {
+export const productReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
     case ActionTypes.SET_PRODUCTS:
       return state
 

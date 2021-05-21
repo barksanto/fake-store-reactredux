11b439e2c4f49d81558a4837c64c7ddd1249d4d3
Barkley Santo <barkley.santo@gmail.com>
2021-05-21 15:26:37 -0500

    second fetch function created with productId added to api call as temp. literal

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -4,10 +4,15 @@ import { useParams } from "react-router-dom";
 // import { useDispatch, useSelector } from "react-redux";
 
 const ProductDetail = () => {
+  //using this id we can make a new api call
+  const { productId } = useParams();
+  // console.log(`Product Id: ${productId}`)
 
-  const { productId } = useParams(); //returns undefined???
-
-  console.log(`Product Id: ${productId}`)
+  const fetchProductDetail = () => {
+    const response = await axios.get(`https:..fakestoreapi.com/products/${productId}`).catch(err => {
+      console.log("Error", err);
+    })
+  }
 
   return (
     <div>

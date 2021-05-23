03b252ac4a4e6c9186751fb79fefe85852874493
Barkley Santo <barkley.santo@gmail.com>
2021-05-23 12:01:52 -0500

    render product card when selected

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -7,11 +7,12 @@ import { selectedProduct } from '../redux/actions/productActions'
 
 const ProductDetail = () => {
   const product = useSelector((state) => state.product);
+  const { image, title, price, category, description } = product;
   //using this id we can make a new api call
   const { productId } = useParams();
   // console.log(`Product Id: ${productId}`)
   const dispatch = useDispatch();
-  console.log(product)
+  // console.log(productId)
 
   const fetchProductDetail = async () => {
     const response = await axios
@@ -28,10 +29,37 @@ const ProductDetail = () => {
   }, [productId])
 
   return (
-    <div>
-      <h1>Product Detail</h1>
+    <div className="ui grid container">
+      {Object.keys(product).length === 0 ? (
+        <div>...Loading</div>
+      ) : (
+        <div className="ui placeholder segment">
+          <div className="ui two column stackable center aligned grid">
+            <div className="ui vertical divider">AND</div>
+            <div className="middle aligned row">
+              <div className="column lp">
+                <img className="ui fluid image" src={image} />
+              </div>
+              <div className="column rp">
+                <h1>{title}</h1>
+                <h2>
+                  <a className="ui teal tag label">${price}</a>
+                </h2>
+                <h3 className="ui brown block header">{category}</h3>
+                <p>{description}</p>
+                <div className="ui vertical animated button" tabIndex="0">
+                  <div className="hidden content">
+                    <i className="shop icon"></i>
+                  </div>
+                  <div className="visible content">Add to Cart</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default ProductDetail;
